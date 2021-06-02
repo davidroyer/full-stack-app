f873dbbada6dc658df8b4b2b64cbabc111be9a7d
David Royer <droyer01@gmail.com>
2021-06-01 20:01:37 -0400

    chore: testing

---
+++ DevNotes.md
@@ -0,0 +1,10 @@
+# DevNotes
+
+## Resources
+
+- <https://github.com/Jordanirabor/isomorphic-application/blob/master/backend/routes/api.js>
+- <https://blog.logrocket.com/jwt-authentication-from-scratch-vue-js-node-js/>
+
+## Video Log
+
+- <https://www.loom.com/share/9e9b3b73de774cbeac1a87db77e8705c>
--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1800s' })
+}
+
+app.get('/login', async (req, res) => {
+  // const SECRET = process.env.TOKEN_SECRET
+
+  const user = {
+    email: 'droyer01@gmail.com'
+  }
+
+  const token = await generateAccessToken(user)
+
+  res.json(token)
+})
 
 app.get('/posts', (req, res) => {
   // res.send('It worked. Send some posts now!')
